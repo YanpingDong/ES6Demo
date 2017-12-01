6acff4c2d9a35c3058ac8906cba72833ef67b5e2
YanpingDong <lear521@163.com>
2017-12-01 17:44:52 +0800

    first commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Component } from 'react';
+import { render } from 'react-dom';
+import { App } from './router';
+
+render(
+  	<App/>,
+  	document.getElementById('root')
+);
---
+++ app/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Component } from 'react';
+import { Link } from 'react-router';
+
+class Home extends Component{
+	render(){
+		return (
+			<div>
+				Home , 
+				<Link to="/about">see about</Link>
+			</div>
+		);
+	}
+}
+
+class About extends Component{
+	render(){
+		return (
+			<h1>my svon</h1>
+		);
+	}
+}
+export { Home , About };
